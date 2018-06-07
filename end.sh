63140a760cb6a60e75fd618591783e869b4f7971
김형모 <hyoungmo@aces.snu.ac.kr>
2018-06-07 14:43:01 +0900

    Update end.sh

--- end.sh
+++ end.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-scp -r /tmp/ppss/ ppta@training01.snu.ac.kr:/home/ppta/ppss/final/
+tar -czvf $USERNAME.tar.gz /tmp/ppss/*.png
+scp $USERNAME.tar.gz ppta@training01.snu.ac.kr:/home/ppta/ppss/final/
+rm $USERNAME.tar.gz
+shutdown now
