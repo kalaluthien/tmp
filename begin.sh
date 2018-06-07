9fbab33b02c5c70eab1aa34b3c85533d9cbd5161
김형모 <hyoungmo@aces.snu.ac.kr>
2018-06-07 14:12:01 +0900

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 dir=/tmp/ppss
-usr=$USERNAME
 
 rm -rf $dir && mkdir -p $dir
-while true; do import -window root $dir/$usr\_`date +%Y%m%d%H%M%S`.png; sleep 10; done &
+while true; do import -window root $dir/$USERNAME\_`date +%Y%m%d%H%M%S`.png; sleep 10; done &
